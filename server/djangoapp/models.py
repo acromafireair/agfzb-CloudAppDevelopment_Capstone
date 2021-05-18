daeb0d5e4e791c0e98f03a1c16b9f59f1ec98896
Kristin Bradley <kristinmariebradley@gmail.com>
2021-05-18 22:49:05 +0000

    Week 3 part 2: made the CarMake and CarModel classes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,22 +4,37 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# Car Make, the __str__ method just returns the name of the car make
+class CarMake(models.Model):
+    name=models.CharField(primary_key=True,max_length=25)
+    description=models.CharField(max_length=150)
+    def __str__(self):
+        return name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-
+# Car Model, the __str__ method returns the car's year, make, and model
+class CarModel(models.Model):
+    CAR_TYPES=[
+        ("SUV", "SUV"),
+        ("Truck", "Truck"),
+        ("Sedan", "Sedan"),
+        ("Van", "Van"),
+        ("Coupe", "Coupe"),
+        ("Wagon", "Wagon"),
+        ("Convertible", "Convertible"),
+        ("Sports Car", "Sports Car"),
+        ("Diesel", "Diesel"),
+        ("Crossover", "Crossover"),
+        ("Luxury Car", "Luxury Car"),
+        ("Hybrid/Electric", "Hybrid/Electric")
+    ]
+    make=models.ManyToManyField(CarMake)
+    name=models.CharField(max_length=50)
+    dealer_id=models.IntegerField()
+    car_type=models.CharField(max_length=20,choices=CAR_TYPES)
+    car_year=models.IntegerField()
+    def __str__(self):
+        return year + " " + make + " " + name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
