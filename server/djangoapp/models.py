916e8b65d7f8f3e239469abed2ba034ec104cd3c
Kristin Bradley <kristinmariebradley@gmail.com>
2021-05-18 23:27:36 +0000

    Week 3 part 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -36,7 +36,59 @@ class CarModel(models.Model):
     def __str__(self):
         return year + " " + make + " " + name
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+# A plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+# A plain Python class `DealerReview` to hold review data
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        # Dealership being reviewed
+        self.dealership = dealership
+        # Name of reviewer
+        self.name = name
+        # Did reviewer purchase the car?
+        self.purchase = purchase
+        # Review text
+        self.review = review
+        # Date reviewer was in dealership
+        self.purchase_date = purchase_date
+        # Make of car
+        self.car_make = car_make
+        # Model of car
+        self.car_model = car_model
+        # Year car was made
+        self.car_year = car_year
+        # Sentiment of review
+        self.sentiment = sentiment
+        # ID number of review
+        self.id = id
+
+    def __str__(self):
+        return "Review: " + self.review
+
+
